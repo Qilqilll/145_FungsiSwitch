240bba11f356c6bb53d325dde02faee0115d606d
Qilqilll <aqilf612@gmail.com>
2024-03-25 10:06:07 +0700

    Add project files.

---
+++ kalkulator pemdas/kalkulator pemdas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+void input()
+{
+    cout << "Bilangan Pertama = ";
+    cin >> a;
+
+    cout << "Bilangan Kedua = "
+
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
